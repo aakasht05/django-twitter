32827c5b793f2d98b27a049517237d91069d7b60
Aakash Tripathi <aakasht05@gmail.com>
2018-03-29 23:13:05 -0400

    Changed from function based to class based views for CRUD operations

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+from .models import Tweet
 
 # Create your views here.
 
+class TweetDetailView(DetailView):
 
-def tweet_detail_view(request, id=1):
-    return render(request, "tweets/detail_view.html", {})
+    queryset = Tweet.objects.all()
 
-def tweet_list_view(request):
-    return render(request, "tweets/list_view.html", {})
+    def get_object(self):
+        return Tweet.objects.get(id=1)
+
+class TweetListView(ListView):
+    queryset = Tweet.objects.all()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(TweetListView, self).get_context_data(*args, **kwargs)
+        print(context)
+        context["another_list"] = Tweet.objects.all()
+        print(context)
+        return context
